fa140f1814e97acb7e9df15adb13d276b45de291
EeliK <eeli.kaikkonen@gmail.com>
2018-01-03 01:02:27 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from complex_plugin_action import ComplexPluginAction
+ComplexPluginAction().register() # Instantiate and register to Pcbnew
+# This is run when the pcb editor is opened
+# Note the relative import! See http://www.diveintopython3.net/porting-code-to-python-3-with-2to3.html#import
